3e5313dea1db0d9962af873cae1c2b068cc99e4f
Priscilla Haibara <megumi.ufrn@gmail.com>
2025-12-30 16:54:57 -0300

    feat(auth): create AuthContext and AuthProvider

--- src/providers/authProvider.tsx
+++ src/providers/authProvider.tsx
@@ -1,4 +1,29 @@
+"use client";
+
+import type { Session, User } from "@supabase/supabase-js";
+import { createContext, useState } from "react";
+
+type AuthContextType = {
+  user: User | null;
+  session: Session | null;
+  loading: boolean;
+  isAuthenticated: boolean;
+};
+
+export const AuthContext = createContext<AuthContextType | undefined>(
+  undefined,
+);
+
 export function AuthProvider({ children }: { children: React.ReactNode }) {
-  // TODO: implement auth logic
-  return <>{children}</>;
+  const [session] = useState<Session | null>(null);
+  const [user] = useState<User | null>(null);
+  const [loading] = useState(false);
+
+  return (
+    <AuthContext.Provider
+      value={{ user, session, loading, isAuthenticated: !!user }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
 }
