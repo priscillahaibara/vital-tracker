1d241ec64c3c747b4d5feab00e7e8f1ebe6c7302
Priscilla Haibara <megumi.ufrn@gmail.com>
2025-12-31 06:10:22 -0300

    feat(auth): add login form UI

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,3 +1,50 @@
+"use client";
+
+import { useState } from "react";
+
 export default function Page() {
-  return <h2>Login Page</h2>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+
+  return (
+    <main className="flex h-screen flex-col items-center justify-center">
+      <form
+        onSubmit={handleSubmit}
+        className="flex w-full max-w-sm flex-col gap-3 px-8 py-4"
+      >
+        <h1 className="mb-2 text-center text-2xl font-bold">
+          Welcome to Vital Tracker
+        </h1>
+
+        <input
+          type="email"
+          required
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          className="rounded-md border border-neutral-400 px-3 py-1.5"
+        />
+
+        <input
+          type="password"
+          required
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          className="rounded-md border border-neutral-400 px-3 py-1.5"
+        />
+
+        <button
+          type="submit"
+          className="rounded-md border bg-neutral-800 px-3 py-1.5 font-semibold text-white disabled:cursor-not-allowed disabled:opacity-50"
+        >
+          Login
+        </button>
+      </form>
+    </main>
+  );
 }
