643e2e23a409b321c4ccb864af25926f4b6584e2
Priscilla Haibara <megumi.ufrn@gmail.com>
2026-01-11 12:24:14 -0300

    feat(auth): add basic route to register page

---
+++ src/app/(public)/register/page.tsx
@@ -0,0 +1,12 @@
+export default function Page() {
+  return (
+    <main>
+      <h1>Create your account</h1>
+      <p>
+        Complete your signup using the invitation link you received in your
+        email.
+      </p>
+      {/* Form will be implemented in the next commit */}
+    </main>
+  );
+}
