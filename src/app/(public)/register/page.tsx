4689214ebbf2db6a540ebd8472567a6be356db7f
Priscilla Haibara <megumi.ufrn@gmail.com>
2026-01-12 20:28:48 -0300

    feat(auth): add password register form

--- src/app/(public)/register/page.tsx
+++ src/app/(public)/register/page.tsx
@@ -1,12 +1,40 @@
+"use client";
+
+import { Button } from "@/components/ui/Button";
+import { Input } from "@/components/ui/Input";
+
 export default function Page() {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+
   return (
-    <main>
-      <h1>Create your account</h1>
-      <p>
-        Complete your signup using the invitation link you received in your
-        email.
-      </p>
-      {/* Form will be implemented in the next commit */}
+    <main className="flex h-screen flex-col items-center justify-center">
+      <form
+        onSubmit={handleSubmit}
+        className="flex w-full max-w-sm flex-col gap-3 px-8 py-4"
+      >
+        <h1 className="mb-2 text-center text-2xl font-bold">
+          Create your account
+        </h1>
+        <p>
+          Complete your signup using the invitation link you received in your
+          email.
+        </p>
+        <Input
+          type="password"
+          placeholder="Set password"
+          minLength={6}
+          required
+        />
+        <Input
+          type="password"
+          placeholder="Confirm password"
+          minLength={6}
+          required
+        />
+        <Button type="submit">Create Account</Button>
+      </form>
     </main>
   );
 }
