6e99d6baca451ed3f3be1418bf9578f21502c819
Priscilla Haibara <megumi.ufrn@gmail.com>
2026-01-22 20:32:14 -0300

    feat(auth): add name input and form state

--- src/app/(protected)/profile/setup/page.tsx
+++ src/app/(protected)/profile/setup/page.tsx
@@ -1,7 +1,18 @@
+"use client";
+
+import { useState } from "react";
 import { Button } from "@/components/ui/Button";
 import { Input } from "@/components/ui/Input";
 
 export default function Page() {
+  const [name, setName] = useState("");
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
+    e.preventDefault();
+
+    console.log("Submitting name");
+  };
+
   return (
     <div className="max-w-md">
       <h1 className="mb-3 text-2xl font-bold">Complete Profile</h1>
@@ -10,11 +21,15 @@ export default function Page() {
         information.
       </p>
 
-      <form className="mb-2 flex gap-2">
-        <Input type="text" placeholder="Full name" disabled className="flex-1" />
-        <Button type="submit" disabled>
-          Submit
-        </Button>
+      <form onSubmit={handleSubmit} className="mb-2 flex gap-2">
+        <Input
+          type="text"
+          placeholder="Full name"
+          className="flex-1"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <Button type="submit">Submit</Button>
       </form>
     </div>
   );
